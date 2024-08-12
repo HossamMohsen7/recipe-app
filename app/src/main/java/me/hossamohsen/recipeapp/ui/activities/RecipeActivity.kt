e835c11eb78c368a695220fac6608ebebab68a98
Hossam Mohsen <6911267+HossamMohsen7@users.noreply.github.com>
2024-08-12 20:08:05 +0300

    feat: add required activities with their layout files

--- app/src/main/java/me/hossamohsen/recipeapp/ui/activities/RecipeActivity.kt
+++ app/src/main/java/me/hossamohsen/recipeapp/ui/activities/RecipeActivity.kt
@@ -1,16 +1,17 @@
-package me.hossamohsen.recipeapp
+package me.hossamohsen.recipeapp.ui.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import me.hossamohsen.recipeapp.R
 
-class MainActivity : AppCompatActivity() {
+class RecipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_recipe)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
