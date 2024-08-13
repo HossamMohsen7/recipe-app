3e225d71faa1ef80711aeb87707afe3f3b01ad28
Hossam Mohsen <6911267+HossamMohsen7@users.noreply.github.com>
2024-08-13 04:42:56 +0300

    fix(auth): fix RecipeActivity navigating back to splash and change getUserById query

--- app/src/main/java/me/hossamohsen/recipeapp/ui/fragments/splash/SplashViewModel.kt
+++ app/src/main/java/me/hossamohsen/recipeapp/ui/fragments/splash/SplashViewModel.kt
@@ -1,5 +1,6 @@
 package me.hossamohsen.recipeapp.ui.fragments.splash
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
@@ -8,6 +9,7 @@ import me.hossamohsen.recipeapp.data.local.AppDatabase
 import me.hossamohsen.recipeapp.data.local.SharedPreferencesManager
 import me.hossamohsen.recipeapp.data.repository.UserRepository
 import me.hossamohsen.recipeapp.data.state.UserState
+import java.util.UUID
 
 class SplashViewModel : ViewModel() {
 
@@ -17,8 +19,9 @@ class SplashViewModel : ViewModel() {
         viewModelScope.launch {
             delay(3000)
             val loggedUserId = SharedPreferencesManager.getString(SharedPreferencesManager.KEY_USER_ID)
+            Log.i("SplashViewModel", "Logged user id: $loggedUserId")
             if (loggedUserId != null) {
-                val user = userRepository.getUserById(loggedUserId)
+                val user = userRepository.getUserById(UUID.fromString(loggedUserId))
                 if (user == null) {
                     SharedPreferencesManager.saveString(SharedPreferencesManager.KEY_USER_ID, null)
                     callback(UserState.NotLoggedIn)
